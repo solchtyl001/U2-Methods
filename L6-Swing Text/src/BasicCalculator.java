9eb36e7474c9f79f23f54903fec08c722faacc00
solchtyl001 <solchtyl001@warren.k12.in.us>
2021-09-30 13:09:39 -0400

    calculator

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
 public class BasicCalculator {
 
     /* Create a calculator program with two text fields
@@ -8,4 +15,128 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+    static JLabel answerLabel;
+    static JLabel answerLabel2;
+    static JTextField textInputField2;
+    static JTextField textInputField;
+    static JLabel result;
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("calclature");
+        JPanel panel = new JPanel();
+        JButton multiply = new JButton("multiply");
+        JButton divide = new JButton("divide");
+        JButton add = new JButton("add");
+        JButton subtract = new JButton("subtract");
+
+        textInputField = new JTextField(10);
+        textInputField2= new JTextField(10);
+        answerLabel = new JLabel("enter a number");
+        answerLabel2 = new JLabel("enter a number");
+        result = new JLabel("0.00");
+        window.setSize(200, 300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel.add(answerLabel);
+        panel.add(textInputField);
+        panel.add(answerLabel2);
+        panel.add(textInputField2);
+
+        panel.add(result);
+
+        window.add(panel);
+        panel.add(multiply);
+        panel.add(divide);
+        panel.add(add);
+        panel.add(subtract);
+
+        window.setVisible(true);
+
+        multiply.addActionListener(new Multiplyer());
+        add.addActionListener(new adder());
+        subtract.addActionListener(new subtractor());
+        divide.addActionListener(new divider());
+    }
+
+        public static class Multiplyer implements ActionListener{
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                double numberOne = Double.parseDouble(textInputField.getText());
+                double numberTwo = Double.parseDouble(textInputField2.getText());
+
+                result.setText(String.valueOf(numberOne*numberTwo));
+            }
+        }
+
+
+    public static class divider implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            double numberOne = Double.parseDouble(textInputField.getText());
+            double numberTwo = Double.parseDouble(textInputField2.getText());
+
+            result.setText(String.valueOf(numberOne/numberTwo));
+
+        }
+    }
+
+
+    public static class subtractor implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            double numberOne = Double.parseDouble(textInputField.getText());
+            double numberTwo = Double.parseDouble(textInputField2.getText());
+
+            result.setText(String.valueOf(numberOne-numberTwo));
+
+
+        }
+    }
+
+
+    public static class adder implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            double numberOne = Double.parseDouble(textInputField.getText());
+            double numberTwo = Double.parseDouble(textInputField2.getText());
+
+            result.setText(String.valueOf(numberOne+numberTwo));
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- L6-Swing Text/src/SimpleInput.java
+++ L6-Swing Text/src/SimpleInput.java
@@ -1,4 +1,79 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class SimpleInput {
+
+    static  int ClickerCount = 0;
+    static JLabel clickerCountLabel ;
+    static JTextField textInputField;
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("clicker game");
+        JPanel panel = new JPanel();
+        textInputField = new JTextField(10);
+       clickerCountLabel = new JLabel("BALANCE: $");
+       JLabel textFieldInput = new JLabel(" enter multplyer");
+        JButton clicker = new JButton("click fo a dolla");
+
+        window.setSize(150, 200);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        clicker.addActionListener(new ClickerListener());
+
+        panel.add(textFieldInput);
+        panel.add(textInputField);
+        panel.add(clickerCountLabel);
+        panel.add(clicker);
+        window.add(panel);
+
+        window.setVisible(true);
+
+
+    }
+
+
+    private static class ClickerListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            int multiplyer = Integer.parseInt(textInputField.getText());
+
+            ClickerCount+= multiplyer;
+        clickerCountLabel.setText("balance: $" + ClickerCount);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
